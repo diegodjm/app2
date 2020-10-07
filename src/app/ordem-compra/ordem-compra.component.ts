ca5df5423671365c07a9ff22486a47fccb00a3ff
diegodjm <diego.djm@hotmail.com>
2020-10-07 10:16:42 -0300

    templates forms
    templates forms

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { OrdemCompraService } from '../ordem-compra.service'
+import { Pedido } from '../shared/pedido.model';
 
 @Component({
   selector: 'app-ordem-compra',
@@ -9,9 +11,31 @@ import { OrdemCompraService } from '../ordem-compra.service'
 })
 export class OrdemCompraComponent implements OnInit {
 
+  @ViewChild('formulario', null) public f: NgForm
+
+  public idPedidoCompra: number
+
   constructor(private ordemCompraService: OrdemCompraService) { }
 
   ngOnInit() {
     
   }
-}
+
+  public confirmarCompra(): void {
+    console.log(this.f.value)
+    
+    let pedido: Pedido = new Pedido(
+      this.f.value.endereco,
+      this.f.value.numero,
+      this.f.value.complemento,
+      this.f.value.formaPagamento
+    )
+
+    this.ordemCompraService.efetivarCompra(pedido)
+    .subscribe((idPedido: number) => {
+      this.idPedidoCompra = idPedido
+      //console.log('Pedido cadastrado com sucesso! Id do pedido: ' + idPedido)
+    })
+
+  }
+} 
